d6030a2baeb096d1415098a292ed66427c02b902
Adam Noakes <adam.noakes@me.com>
2016-02-20 19:18:42 +0000

    Initial test update

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import pyshark
 
-capture = pyshark.LiveCapture(interface='en0')
-capture.sniff(timeout=50)
+capture = pyshark.LiveCapture(interface='wlan0', display_filter='wlan.fc.type_subtype eq 4')
+capture.set_debug()
+capture.sniff(timeout=5)
 
 for packet in capture.sniff_continuously(packet_count=5):
     print 'Just arrived:', packet
