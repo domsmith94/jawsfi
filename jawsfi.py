801e8120eba5e4d8374796359e31154f6b244ced
Adam Noakes <adam.noakes@me.com>
2016-02-21 12:50:59 +0000

    Updated test and basic jawsfi channel hopping code

---
+++ jawsfi.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+from threading import Thread, Lock
+from subprocess import Popen, PIPE
+from signal import SIGINT, signal
+import argparse
+import os
+import sys
+
+# Console colors
+W = '\033[0m'  # white
+R = '\033[31m' # red
+
+# Get arguments
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i",
+    			"--interface",
+                        help="Choose monitor mode interface. \
+                                Example: -i mon5",
+    			required=True)
+
+    parser.add_argument("-c",
+                        "--channel",
+                        help="Listen for probe requests only on the specified channel. \
+                                Example: -c 6")
+    return parser.parse_args()
+
+'''
+Adapted from wifijammer.py
+'''
+# Hop channel loop
+def channel_hop(args):
+
+    channelNum = 0
+    maxChan = 11
+    err = None
+
+    while 1:
+        if args.channel:
+            with lock:
+               channelNum = args.channel
+        else:
+            channelNum +=1
+            if channelNum > maxChan:
+                channelNum = 1
+
+            try:
+                proc = Popen(['iw', 'dev', args.interface, 'set', 'channel', str(channelNum)], stdout=DN, stderr=PIPE)
+            except OSError:
+                print '['+R+'-'+W+'] Could not execute "iw"'
+                os.kill(os.getpid(),SIGINT)
+                sys.exit(1)
+            for line in proc.communicate()[1].split('\n'):
+                if len(line) > 2: # iw dev shouldnt display output unless there's an error
+                    err = '['+R+'-'+W+'] Channel hopping failed: '+R+line+W
+        
+        if err:
+            print err
+	if args.channel:
+            time.sleep(.05)
+
+# Shutdown jawsfi
+def stop(signal, frame):
+    sys.exit('Closing')
+
+# Run
+if __name__ == "__main__":
+    if os.geteuid():
+        sys.exit('['+R+'-'+W+'] Run using sudo.')
+    args = parse_args()
+    interface = args.interface
+    DN = open(os.devnull, 'w')
+    
+    # Start channel hopping thread
+    hop = Thread(target=channel_hop, args=(args,))
+    hop.daemon = True
+    hop.start()
+
+    signal(SIGINT, stop)
+    
+    # Packet sniffing code
+    while 1:
+        continue
--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import pyshark
 
-capture = pyshark.LiveCapture(interface='wlan0', display_filter='wlan.fc.type_subtype eq 4')
-capture.set_debug()
-capture.sniff(timeout=5)
+'''
+File used for testing out functions
+'''
 
-for packet in capture.sniff_continuously(packet_count=5):
-    print 'Just arrived:', packet
+capture = pyshark.LiveCapture(interface='wlan0', display_filter='wlan.fc.type_subtype eq 4')
+#capture.set_debug()
+
+print 'Capturing all probe requests...'
+for packet in capture.sniff_continuously(packet_count=50):
+    print 'Device: ', packet.wlan.ta_resolved, ' Signal: ', packet.radiotap.dbm_antsignal ,'db'
