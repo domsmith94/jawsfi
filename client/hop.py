b0fe68ac90838b5aa8b91854e26b8015080f7706
domsmith94 <domsmith94@gmail.com>
2016-02-24 13:38:50 +0000

    indentation fix

--- client/hop.py
+++ client/hop.py
@@ -7,47 +7,47 @@ import os
 import sys
 
 class HoppingThread(Thread):
-    # This is the thread we will use for channel HoppingThread
+	# This is the thread we will use for channel HoppingThread
 
-    def __init__(self, args):
-        super(HoppingThread, self).__init__()
-        self._stop = threading.Event()
+	def __init__(self, args):
+		super(HoppingThread, self).__init__()
+		self._stop = threading.Event()
 	self.args = args
-    	self.DN = open(os.devnull, 'w')
-    def stop(self):
-        self._stop.set()
+		self.DN = open(os.devnull, 'w')
+	def stop(self):
+		self._stop.set()
 
-    def stopped(self):
-        return self._stop.isSet()
+	def stopped(self):
+		return self._stop.isSet()
 
-    def run(self):
+	def run(self):
 
-        channelNum = 0
-        maxChan = 11
-        err = None
+		channelNum = 0
+		maxChan = 11
+		err = None
 
-        while not self.stopped():
+		while not self.stopped():
 
-            if self.args.channel:
-                with lock:
-                   channelNum = self.args.channel
-            else:
-                channelNum +=1
-                if channelNum > maxChan:
-                    channelNum = 1
+			if self.args.channel:
+				with lock:
+				   channelNum = self.args.channel
+			else:
+				channelNum +=1
+				if channelNum > maxChan:
+					channelNum = 1
 
-                try:
-                    if not self.stopped():
-		    	proc = Popen(['iw', 'dev', self.args.interface, 'set', 'channel', str(channelNum)], stdout=self.DN, stderr=PIPE)
-                except OSError:
-                    print '['+R+'-'+W+'] Could not execute "iw"'
-                    os.kill(os.getpid(),SIGINT)
-                    sys.exit(1)
-                for line in proc.communicate()[1].split('\n'):
-                    if len(line) > 2: # iw dev shouldnt display output unless there's an error
-                        err = '['+R+'-'+W+'] Channel hopping failed: '+R+line+W
-            
-            if err:
-                print err
-        if self.args.channel:
-                time.sleep(.05)
+				try:
+					if not self.stopped():
+				proc = Popen(['iw', 'dev', self.args.interface, 'set', 'channel', str(channelNum)], stdout=self.DN, stderr=PIPE)
+				except OSError:
+					print '['+R+'-'+W+'] Could not execute "iw"'
+					os.kill(os.getpid(),SIGINT)
+					sys.exit(1)
+				for line in proc.communicate()[1].split('\n'):
+					if len(line) > 2: # iw dev shouldnt display output unless there's an error
+						err = '['+R+'-'+W+'] Channel hopping failed: '+R+line+W
+			
+			if err:
+				print err
+		if self.args.channel:
+				time.sleep(.05)
--- client/jawsfi.py
+++ client/jawsfi.py
@@ -16,65 +16,65 @@ G = '\033[32m' # green
 
 # Get arguments
 def parse_args():
-    parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i",
-    			"--interface",
-                        help="Choose monitor mode interface. \
-                                Example: -i mon5",
-    			required=True)
+	parser.add_argument("-i",
+				"--interface",
+						help="Choose monitor mode interface. \
+								Example: -i mon5",
+				required=True)
 
-    parser.add_argument("-c",
-                        "--channel",
-                        help="Listen for probe requests only on the specified channel. \
-                                Example: -c 6")
-    return parser.parse_args()
+	parser.add_argument("-c",
+						"--channel",
+						help="Listen for probe requests only on the specified channel. \
+								Example: -c 6")
+	return parser.parse_args()
 
 def enable_monitor(interface):
-    print '['+G+'+'+W+'] Starting monitor mode for '+G+interface+W
-    try:
-        os.system('ifconfig %s down' % interface)
-        os.system('iwconfig %s mode monitor' % interface)
-        os.system('ifconfig %s up' % interface)
-    except Exception:
-        sys.exit('['+R+'-'+W+'] Could not start monitor mode')
+	print '['+G+'+'+W+'] Starting monitor mode for '+G+interface+W
+	try:
+		os.system('ifconfig %s down' % interface)
+		os.system('iwconfig %s mode monitor' % interface)
+		os.system('ifconfig %s up' % interface)
+	except Exception:
+		sys.exit('['+R+'-'+W+'] Could not start monitor mode')
 
 def disable_monitor(interface):
-    print '['+R+'-'+W+'] Stopping monitor mode for '+R+interface+W
-    try:
-        os.system('ifconfig %s down' % interface)
-        os.system('iwconfig %s mode managed' % interface)
-        os.system('ifconfig %s up' % interface)
-    except Exception:
-        sys.exit('['+R+'-'+W+'] Could not disable monitor mode')
+	print '['+R+'-'+W+'] Stopping monitor mode for '+R+interface+W
+	try:
+		os.system('ifconfig %s down' % interface)
+		os.system('iwconfig %s mode managed' % interface)
+		os.system('ifconfig %s up' % interface)
+	except Exception:
+		sys.exit('['+R+'-'+W+'] Could not disable monitor mode')
 
 # Shutdown jawsfi
 def stop(signal, frame):
-    hop.stop()
-    disable_monitor(interface)
-    sys.exit('Closing')
+	hop.stop()
+	disable_monitor(interface)
+	sys.exit('Closing')
 
 # Run
 if __name__ == "__main__":
-    if os.geteuid():
-        sys.exit('['+R+'!'+W+'] Run using sudo.')
-    args = parse_args()
-    interface = args.interface
-    
-    # Start the interface
-    enable_monitor(interface)
+	if os.geteuid():
+		sys.exit('['+R+'!'+W+'] Run using sudo.')
+	args = parse_args()
+	interface = args.interface
+	
+	# Start the interface
+	enable_monitor(interface)
 
-    # Start channel hopping thread
-    hop = HoppingThread(args)
-    hop.start()
+	# Start channel hopping thread
+	hop = HoppingThread(args)
+	hop.start()
 
-    signal(SIGINT, stop)
-    
-    # Packet sniffing code
-    capture = pyshark.LiveCapture(interface=interface, display_filter='wlan.fc.type_subtype eq 4')
-    #capture.set_debug()
+	signal(SIGINT, stop)
+	
+	# Packet sniffing code
+	capture = pyshark.LiveCapture(interface=interface, display_filter='wlan.fc.type_subtype eq 4')
+	#capture.set_debug()
 
-    print 'Capturing all probe requests...'
-    for packet in capture.sniff_continuously():
-	print 'Device: ', packet.wlan.ta_resolved, ' Signal: ', packet.radiotap.dbm_antsignal ,'db'
-    stop(None, None)
+	print 'Capturing all probe requests...'
+	for packet in capture.sniff_continuously():
+		print 'Device: ', packet.wlan.ta_resolved, ' Signal: ', packet.radiotap.dbm_antsignal ,'db'
+	stop(None, None)
