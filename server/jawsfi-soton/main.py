fae0c50092a55e025967c3193c193fa1fbef65d1
domsmith94 <domsmith94@gmail.com>
2016-03-02 15:55:48 +0000

    working json communication

--- server/jawsfi-soton/main.py
+++ server/jawsfi-soton/main.py
@@ -16,6 +16,9 @@
 #
 import webapp2
 import json
+import ast
+from models import ResultSet
+from models import Result
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -23,10 +26,20 @@ class MainHandler(webapp2.RequestHandler):
 
 class DataHandler(webapp2.RequestHandler):
 	def post(self):
-		jsonstring = self.request.body
-		jsonobject = json.loads(jsonstring)
-		print jsonobject
-		self.response.write('This was a resounding success')
+		jsonobject = json.loads(self.request.body)
+		pythonobject = ast.literal_eval(jsonobject)
+
+		if jsonobject:
+			result_set = ResultSet()
+			result_set_key = result_set.put()
+
+			
+			print type(jsonobject)
+			for key, value in pythonobject.iteritems():
+				db_result = Result(mac_address=key, signal=value, result_set=result_set_key)
+				db_result.put()
+
+			self.response.write('Should of put items in data store')
 
 
 app = webapp2.WSGIApplication([
--- server/jawsfi-soton/models.py
+++ server/jawsfi-soton/models.py
@@ -2,13 +2,13 @@ from google.appengine.ext import ndb
 
 class Pi(ndb.Model):
     """A model for storing Rasberry Pi devices"""
-    pi_id = ndb.StringProperty(required=true)
+    pi_id = ndb.StringProperty(required=True)
     created = ndb.DateTimeProperty(auto_now_add=True)
 
 class Result(ndb.Model):
-	mac_address = ndb.StringProperty(required=true)
-	signal = ndb.IntegerProperty(required=true)
+	mac_address = ndb.StringProperty(required=True)
+	signal = ndb.IntegerProperty(required=True)
 	result_set = ndb.KeyProperty()
 
 class ResultSet(ndb.Model):
-	created = ndb.DateTimeProperty
+	created = ndb.DateTimeProperty(auto_now_add=True)
