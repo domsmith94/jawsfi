e7d6c30eaa2d035df479402aedefce84565db46c
domsmith94 <domsmith94@gmail.com>
2016-03-02 13:16:44 +0000

    [server] added pi class and application

--- server/jawsfi-aws/application.py
+++ server/jawsfi-aws/application.py
@@ -1,32 +1,33 @@
 from flask import Flask
-
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>jawsfi AWS</title> </head>\n<body>'''
-instructions = '''
-    <p>Welcome to JAWSFI Server infrastructure</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+import pi
 
 # EB looks for an 'application' callable by default.
-application = Flask(__name__)
+app = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+@app.route('/')
+def index():
+    return 'Hello'
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@app.route('/register', methods=['POST'])
+def register_pi():
+    if request.method == 'POST':
+        print 'We recieved a POST request on register'
+        content = request.json
+        print content
+
+
+@app.route('/send-data', methods=['POST'])
+def send_data():
+    if request.method == 'POST':
+        do_the_login()
+        content = request.json
+        print 'We recieved a POST request on send-data'
+        return 'POST request for send data'
+
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
