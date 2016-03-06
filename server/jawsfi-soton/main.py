a404363582b24665484a0226a7b7153db968af95
domsmith94 <domsmith94@gmail.com>
2016-03-06 11:36:20 +0000

    added a view to server side

--- server/jawsfi-soton/main.py
+++ server/jawsfi-soton/main.py
@@ -1,40 +1,38 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import json
 import ast
+import models
+import jinja2
+import os
 from models import ResultSet
 from models import Result
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 class MainHandler(webapp2.RequestHandler):
+	# Used for home page
     def get(self):
-        self.response.write('Welcome to jawsfi')
+    	template_values = {'num_sets': models.get_num_unique_sets(),
+    						'unique_results': models.get_unique_results(),
+    						'last_result': models.get_last_submission()}
+
+    	template = JINJA_ENVIRONMENT.get_template('index.html')
+
+        self.response.write(template.render(template_values))
 
 class DataHandler(webapp2.RequestHandler):
+	#  send-data route is used by Rasberry Pi Client devices to send MAC collections
 	def post(self):
 		jsonobject = json.loads(self.request.body)
-		pythonobject = ast.literal_eval(jsonobject)
+		pythonobject = ast.literal_eval(jsonobject) #ast.literal used to ensure JSON is a Python dict
 
 		if jsonobject:
 			result_set = ResultSet()
 			result_set_key = result_set.put()
 
-			
-			print type(jsonobject)
 			for key, value in pythonobject.iteritems():
 				db_result = Result(mac_address=key, signal=value, result_set=result_set_key)
 				db_result.put()
--- server/jawsfi-soton/models.py
+++ server/jawsfi-soton/models.py
@@ -8,7 +8,28 @@ class Pi(ndb.Model):
 class Result(ndb.Model):
 	mac_address = ndb.StringProperty(required=True)
 	signal = ndb.IntegerProperty(required=True)
-	result_set = ndb.KeyProperty()
+	result_set = ndb.KeyProperty(required=True)
 
 class ResultSet(ndb.Model):
+	# Standard time needs to be in UTC and represents the time of the results
+	# e.g. 06/03/2016 12:00...12:10...12:20...12:30 irrespective of what time it was 
+	# actually recieved and processed
+	standard_time = ndb.DateTimeProperty() 
 	created = ndb.DateTimeProperty(auto_now_add=True)
+
+def get_num_unique_sets():
+	qry = ResultSet.query()
+	return qry.count()
+
+def get_unique_results():
+	qry = Result.query()
+	return qry.count()
+
+def get_last_submission():
+	qry = ResultSet.query().order(-ResultSet.created)
+	results = qry.fetch(limit=1)
+	if results:
+		return results[0].created
+	else:
+		return None
+
